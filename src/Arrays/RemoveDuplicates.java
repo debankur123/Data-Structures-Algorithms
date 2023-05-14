8ec78d806a982a6c454d72dd0536404c90cd308c
Debankur Bhattacharjee <59233731+debankur123@users.noreply.github.com>
2023-05-14 13:13:04 +0530

    Remove duplicate eleemnts from Array

---
+++ src/Arrays/RemoveDuplicates.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] arr = getInputArray();
+        int result = removeDuplicatesInArray(arr);
+        System.out.println("No of duplicate elements are : " + result);
+    }
+    public static int[] getInputArray(){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+    static int removeDuplicatesInArray(int[] arr){
+        int i = 0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[j] != arr[i]){
+                arr[i+1] = arr[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+}
